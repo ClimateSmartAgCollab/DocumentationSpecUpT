dc9aed592faa2ba196e5f7c0dac858dfda60725b
olichose123 <olivier@banville.net>
2024-10-10 14:08:35 -0400

    first publish

---
+++ output/xrefs-history/xrefs-data-1728583693882.js
@@ -0,0 +1,26 @@
+const allXrefs = {
+  "xrefs": [
+    {
+      "externalSpec": "test-1",
+      "term": "Aal",
+      "repoUrl": null,
+      "terms_dir": null,
+      "owner": null,
+      "repo": null,
+      "site": null,
+      "commitHash": null,
+      "content": null
+    },
+    {
+      "externalSpec": "does-not-exist",
+      "term": "Foo",
+      "repoUrl": null,
+      "terms_dir": null,
+      "owner": null,
+      "repo": null,
+      "site": null,
+      "commitHash": null,
+      "content": null
+    }
+  ]
+};
